3b28bf4541204d622448f64acc4d4e9831cc4461
ValentinSinyakin <valentinbrest95@gmail.com>
2022-05-11 15:28:03 +0300

    fix js and css

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
             <div class="keyboard_lights"></div>
             <div class="keyboard_keys">
                 <div class="row">
-                    <div class="keys">(\)</div>
+                    <div class="keys">~</div>
                     <div class="keys">1</div>
                     <div class="keys">2</div>
                     <div class="keys">3</div>
@@ -44,7 +44,7 @@
                     <div class="keys">P</div>
                     <div class="keys">{</div>
                     <div class="keys">}</div>
-                    <div class="keys slash_key">\</div>
+                    <div class="keys slash_key">&#8260;</div>
                 </div>
                 <div class="row">
                     <div class="keys caps_lock_key">Caps Lock</div>
